db4ea5d5931c08025a15067fd30ecfd558a2f1eb
Parindraa <62353445+Parindraa@users.noreply.github.com>
2022-03-08 17:57:24 -0500

    Update debinstall.sh

--- debinstall.sh
+++ debinstall.sh
@@ -5,6 +5,8 @@ sudo apt-get update -y
 
 sudo apt-get install appimagelauncher -y
 
+chmod +x ~/Downloads/Minecraft.AppImage
+
 cp ~/Downloads/Minecraft.AppImage ~/Minecraft-Bedrock-Launcher-Linux-For-Free/
 
 ./Minecraft.AppImage
