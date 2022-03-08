929bff0b748bbc1ba74083ccdb4aab3cd5080294
Parindraa <62353445+Parindraa@users.noreply.github.com>
2022-03-08 17:56:10 -0500

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 yes | sudo pacman -S appimagelauncher
 
+chmod +x ~/Downloads/Minecraft.AppImage
+
 cp ~/Downloads/Minecraft.AppImage ~/Minecraft-Bedrock-Launcher-Linux-For-Free/
 
 ./Minecraft.AppImage
